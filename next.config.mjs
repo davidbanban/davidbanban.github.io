d40fe3dbb54d2381ef09ed6a0a8b4a5f28e1cd92
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2025-02-19 12:19:40 +0800

    Configure next.js as static site

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,23 @@ import createMDX from '@next/mdx';
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: 'export',
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withMDX = createMDX({
